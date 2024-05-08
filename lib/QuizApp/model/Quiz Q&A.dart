270c55456a0f7e80a3f2f2bd930da6660df1b704
nehaljohny <nehaljohny872001@gmail.com>
2024-05-08 16:17:13 +0530

    commit

---
+++ lib/QuizApp/model/Quiz Q&A.dart
@@ -0,0 +1,62 @@
+List<Map<String, dynamic>> queAns = [
+  {
+    'question': "In which city is Hollywood located?",
+    'options': ['Los Angeles', 'New York City', 'Chicago', 'Miami'],
+    'correctAnswer': ' Los Angeles'
+  },
+  {
+    'question': "Who is the director of the film “Citizen Kane,” often considered one of the greatest movies ever made?",
+    'options': ['Orson Welles', 'Alfred Hitchcock', 'Steven Spielberg', 'Martin Scorsese'],
+    'correctAnswer': 'Orson Welles'
+  },
+  {
+    'question': "Who played the character of Luke Skywalker in the original “Star Wars” trilogy?",
+    'options': ['Mark Hamill', 'Harrison Ford',
+      'Carrie Fisher', 'Alec Guinness'],
+    'correctAnswer': 'Mark Hamill'
+  },
+  {
+    'question': "Who directed the film “Pulp Fiction,” which won the Palme d’Or at the 1994 Cannes Film Festival?",
+    'options': ['Quentin Tarantino', 'Stanley Kubrick', 'Steven Spielberg', 'Martin Scorsese'],
+    'correctAnswer': 'Quentin Tarantino'
+  },
+  {
+    'question': "Which film won the Best Picture Oscar at the 2020 Academy Awards?",
+    'options': ['Parasite', 'Joker', '1917', 'The Shape of Water'],
+    'correctAnswer': 'Parasite'
+  },
+  {
+    'question': "Which actress has won the most Academy Awards for Best Actress?",
+    'options': [' Katharine Hepburn', 'Meryl Streep', 'Bette Davis', 'Cate Blanchett'],
+    'correctAnswer': ' Katharine Hepburn'
+  },
+  {
+    'question': " Who directed the film “Gone Girl” based on the novel by Gillian Flynn?",
+    'options': ['David Fincher', 'Christopher Nolan', 'Wes Anderson', 'Denis Villeneuve'],
+    'correctAnswer': 'David Fincher'
+  },
+  {
+    'question': "Which actress became the youngest person to win four Academy Awards by the age of 26?",
+    'options': ['Jodie Foster', 'Jennifer Lawrence', 'Kate Winslet', 'Cate Blanchett'],
+    'correctAnswer': 'Jodie Foster'
+  },
+  {
+    'question': "Who directed the film “The Shawshank Redemption”?",
+    'options': ['Frank Darabont', 'Quentin Tarantino', 'Steven Spielberg', 'Martin Scorsese'],
+    'correctAnswer': 'Frank Darabont'
+  },
+  {
+    'question': "Who won the Academy Award for Best Actor in a Leading Role 2017?",
+    'options': ['Casey Affleck', 'Andrew Garfield', 'Denzel Washington', 'Ryan Gosling'],
+    'correctAnswer': 'Casey Affleck'
+  },
+
+
+
+
+
+
+
+
+
+];
